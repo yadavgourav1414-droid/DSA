03ae04eb9df6b04bb46997cad77b947c5f1e8933
yadavgourav1414-droid <yadavgourav1414@gmail.com>
2026-06-29 11:10:04 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (81.78%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0, n = s.size();
+        while (i < n && s[i] == ' ') {
+            i++;
+        }
+        int sign = 1;
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-') {
+                sign = -1;
+            }
+            i++;
+        }
+        int result = 0;
+
+        while (i < n && isdigit(s[i])) {
+            int digit = s[i] - '0';
+
+            if (result > (INT_MAX - digit) / 10) {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
+            result = result * 10 + digit;
+            i++;
+        }
+        return result * sign;
+    }
+};
